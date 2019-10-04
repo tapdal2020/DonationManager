0ffc7e5d2e5d66174e8086818ea7bef9373002da
Jacob Mink <3.141jvmink@gmail.com>
2019-10-04 16:42:12 -0500

    Working with user logni

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :users
+  resources :sessions, except: [:index, :show, :edit, :update]
+
+  root 'sessions#new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

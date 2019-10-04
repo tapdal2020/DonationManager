18a7725e91dd6df68708984741dfd6e7430273bf
Jacob Mink <3.141jvmink@gmail.com>
2019-10-04 16:30:36 -0500

    Adding users table

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+    
+end

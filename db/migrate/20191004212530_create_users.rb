ed927a585965bb4bf019106dacfc645b8282a507
Jacob Mink <3.141jvmink@gmail.com>
2019-10-07 10:43:43 -0500

    Remove admin column from Users table

--- db/migrate/20191004212530_create_users.rb
+++ db/migrate/20191004212530_create_users.rb
@@ -10,7 +10,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :city
       t.string :state
       t.string :zip_code
-      t.boolean :admin, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_012450) do
-
-  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2019_10_04_212530) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
@@ -27,7 +22,6 @@ ActiveRecord::Schema.define(version: 2019_10_06_012450) do
     t.string "city"
     t.string "state"
     t.string "zip_code"
-    t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

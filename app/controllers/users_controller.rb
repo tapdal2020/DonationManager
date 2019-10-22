eb39dd890d1011a9fda33ed4eaeb7415ecd6be97
Jacob Mink <3.141jvmink@gmail.com>
2019-10-22 14:48:48 -0500

    Creating, testing Admin CRUD users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!, except: [:new, :create]
+    before_action :authenticate_admin!, only: [:index]
     helper_method :sort_column, :sort_direction
     
     def new
@@ -7,12 +8,20 @@ class UsersController < ApplicationController
     end
 
     def create
+        if current_user && !current_user.admin?
+            redirect_to user_path(session[:user_id]) and return
+        end
+
+        if !params["user"][:admin].nil? && !current_user
+            redirect_to new_session_path and return
+        end
+
         @user = User.new(user_params)
         
         if @user.save(context: current_admin ? :admin : :user)
-            redirect_to new_session_path
+            redirect_to new_session_path and return
         else
-            render 'new'
+            render 'new' and return
         end
     end
 
@@ -22,10 +31,11 @@ class UsersController < ApplicationController
 
     def show
         # make sure the user is NOT accessing another user's page
+        # but allow admin to do so
         request = params[:id].to_i
         with = session[:user_id].to_i
-        if (request != with)
-            render 'unauthorized'
+        if request != with && !current_admin
+            render 'unauthorized' and return
         else
             @html_donation_title = (is_currently_admin?) ? 'Donation Administrator' : 'Donations Overview'
             @my_donations = (is_currently_admin?) ? MadeDonation.all : MadeDonation.where("user_id = ?", request)
@@ -34,11 +44,23 @@ class UsersController < ApplicationController
     end
 
     def edit
+        request = params[:id].to_i
+        with = session[:user_id].to_i
+        if request != with && !current_admin
+            render 'unauthorized' and return
+        end
 
+        @user = user.find(params[:id])
     end
 
     def update
-
+        request = params[:id].to_i
+        with = session[:user_id].to_i
+        if request != with && !current_admin
+            render 'unauthorized' and return
+        end
+        
+        @user.save(update_params)
     end
 
     private
@@ -50,7 +72,7 @@ class UsersController < ApplicationController
     end
 
     def is_currently_admin?
-        User.find(params[:id]).admin?
+        current_admin
     end
 
     def user_params
@@ -58,6 +80,14 @@ class UsersController < ApplicationController
             params.require("user").permit(:email, :password, :password_confirmation, :admin)
         else
             params.require("user").permit(:first_name, :last_name, :email, :password, :password_confirmation, :street_address_line_1, :city, :state, :zip_code, :street_address_line_2)
+        end
+    end
+
+    def update_params
+        if current_admin
+            params.require("user").permit(:first_name, :last_name, :email, :street_address_line_1, :city, :state, :zip_code, :street_address_line_2, :admin)
+        else
+            params.require("user").permit(:first_name, :last_name, :email, :street_address_line_1, :city, :state, :zip_code, :street_address_line_2)
         end
     end
     

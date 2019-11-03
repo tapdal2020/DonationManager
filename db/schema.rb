4f6f8981b09b75efc75b01a370347c5a5b734f81
Jacob Mink <3.141jvmink@gmail.com>
2019-11-03 10:27:43 -0600

    Fixed broken test when calling MadeDonation.joins(:user)

--- db/schema.rb
+++ db/schema.rb
@@ -12,26 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2019_10_28_221956) do
 
-  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "made_donations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id"
     t.string "payment_id", null: false
     t.decimal "price", precision: 6, scale: 2, default: "0.0"
     t.string "payer_id"
     t.string "token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_made_donations_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

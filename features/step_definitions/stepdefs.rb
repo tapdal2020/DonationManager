49e7bf2c50d3ba362c53ca23e398fea8b04f55c3
Jacob Mink <3.141jvmink@gmail.com>
2019-11-14 13:20:04 -0600

    Finishing test for user-crud-user

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -126,7 +126,11 @@ Given "I have received and followed a password reset link" do
 end
 
 When(/^I fill in "(.*)" with "(.*)"$/) do |label, entry|
-    fill_in(label, with: entry)
+    if label == 'user_state'
+        find(:select, label).find(:option, entry).select_option
+    else
+        fill_in(label, with: entry)
+    end
 end
 
 When(/^I press "(.*)"$/) do |button|
@@ -142,6 +146,16 @@ When (/^I fill in new (user|admin) information$/) do |role|
     
     check('user_admin') if role == 'admin'
     find(:select, 'user_state').find(:option, 'TX').select_option
+
+    entries.each do |item, value|
+        fill_in item, with: value
+    end
+end
+
+When (/^I fill in update user information$/) do 
+    entries = { 'user_first_name' => 'FirstName', 'user_last_name' => 'LastName', 'user_email' => 'firstlast@email.com', 'user_street_address_line_1' => 'home', 'user_city' => 'College Station', 'user_zip_code' => '77840' }
+    
+    find(:select, 'user_state').find(:option, 'AZ').select_option
 
     entries.each do |item, value|
         fill_in item, with: value

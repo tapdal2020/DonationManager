6864d7afa0b4199224d01bde9bb23c20620c1852
Jacob Mink <3.141jvmink@gmail.com>
2019-11-21 08:46:37 -0600

    Fixed zip code validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     
     validates :first_name, :last_name, :street_address_line_1, :city, :state, :zip_code, presence: true, on: :user
-    validates_numericality_of :zip_code, on: :create, greater_than_or_equal_to: 0, only_integer: true
+    validates :zip_code, numericality: { greater_than_or_equal_to: 0, only_integer: true }, on: [:create, :user]
     
     validates :email, presence: true, uniqueness: { case_sensitive: true }
     validates_email_format_of :email, message: "Invalid email"

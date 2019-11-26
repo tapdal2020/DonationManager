a616189ed1f58df9b8cbd12c94fd2ac0bd8eb775
Clayton Draughon <claytondragon@gmail.com>
2019-11-26 15:02:41 +0000

    Clayton work on flashes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,8 +16,8 @@ class SessionsController < ApplicationController
             session[:rememberme] = session_info["rememberme"]
             redirect_to user_path(user.id) and return
         else
-            flash.now[:alert] = "Invalid email or password"
-            render 'new' and return
+            flash[:alert] = "Invalid email or password"
+            redirect_to new_session_path and return
         end
     end
 

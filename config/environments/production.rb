5224ebbec48fffec3a2beb521ef6a935e49ddac0
Jacob Mink <3.141jvmink@gmail.com>
2019-12-06 15:58:40 -0600

    Fixing typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { host: mail.bvjazz.org }
+  config.action_mailer.default_url_options = { host: 'mail.bvjazz.org' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
